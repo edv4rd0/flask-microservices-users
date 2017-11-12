7d2a85f271592587a980849c9cebb3bdbe6e759e
Edward Williams <edward.pendragon@gmail.com>
2017-11-13 12:20:00 +1300

    Adds testing

--- manage.py
+++ manage.py
@@ -1,9 +1,20 @@
+import unittest
 from flask_script import Manager
 
 from project import app, db
 
 
 manager = Manager(app)
+
+
+@manager.command
+def test():
+    """Runs the tests without code coverage."""
+    tests = unittest.TestLoader().discover('project/tests', pattern='test*.py')
+    result = unittest.TextTestRunner(verbosity=2).run(tests)
+    if result.wasSuccessful():
+        return 0
+    return 1
 
 
 @manager.command
